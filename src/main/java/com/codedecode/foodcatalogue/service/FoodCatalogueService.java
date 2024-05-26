d8ec7db43be7568eb9ecabd68720b460de0eda67
Mopendra Kumar <mopendra.athe@gmail.com>
2024-05-26 16:31:28 +0530

    first commit
    first commit

---
+++ src/main/java/com/codedecode/foodcatalogue/service/FoodCatalogueService.java
@@ -0,0 +1,50 @@
+package com.codedecode.foodcatalogue.service;
+
+import com.codedecode.foodcatalogue.dto.FoodCataloguePage;
+import com.codedecode.foodcatalogue.dto.FoodItemDTO;
+import com.codedecode.foodcatalogue.dto.Restaurant;
+import com.codedecode.foodcatalogue.entity.FoodItem;
+import com.codedecode.foodcatalogue.mapper.FoodItemMapper;
+import com.codedecode.foodcatalogue.repo.FoodItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class FoodCatalogueService {
+
+    @Autowired
+    FoodItemRepo foodItemRepo;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
+        FoodItem foodItemSavedInDB = foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
+        return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSavedInDB);
+    }
+
+
+    public FoodCataloguePage  fetchFoodCatalougePageDetail(Integer restaurantId) {
+        List<FoodItem> foodItemList = fetchFoodItemList(restaurantId);
+        Restaurant restaurant = fetchRestaurantDetailsFromRestaurantMS(restaurantId);
+        return creatFoodCataloguePage(foodItemList, restaurant);
+    }
+
+    private FoodCataloguePage creatFoodCataloguePage(List<FoodItem> foodItemList, Restaurant restaurant) {
+        FoodCataloguePage foodCataloguePage =  new FoodCataloguePage();
+        foodCataloguePage.setFoodItemsList(foodItemList);
+        foodCataloguePage.setRestaurant(restaurant);
+        return foodCataloguePage;
+    }
+
+    private Restaurant fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
+        return restTemplate.getForObject("http://RESTAURANT-SERVICE/restaurant/fetchById/"+restaurantId, Restaurant.class);
+    }
+
+    private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
+        return foodItemRepo.findByRestaurantId(restaurantId);
+    }
+}
